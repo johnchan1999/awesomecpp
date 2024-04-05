4a9f7fd5b850d9d819e9969d27ad6a20e69792c6
johnchan1999 <jun2428941453@gmail.com>
2024-04-06 01:59:02 +0800

    last submit

--- TestPC.cc
+++ TestPC.cc
@@ -2,7 +2,7 @@
 #include "Producer.h"
 #include "TaskQueue.h"
 #include "Thread.h"
-
+#include <memory>
 using std::bind;
 void test() {
   TaskQueue taskQue(10);
@@ -17,8 +17,20 @@ void test() {
   mth1.stop();
   mth2.stop();
 }
+void test1() {
+  TaskQueue taskQue(20);
+  Producer pro(taskQue);
+  Consumer con(taskQue);
+  std::shared_ptr<Thread> mth1(new Thread(bind(&Producer::process, &pro)));
+  std::unique_ptr<Thread> mth2(new Thread(bind(&Consumer::process, &con)));
 
+  mth1->start();
+  mth2->start();
+
+  mth1->stop();
+  mth2->stop();
+}
 int main(int argc, char *argv[]) {
-  test();
+  test1();
   return 0;
 }
